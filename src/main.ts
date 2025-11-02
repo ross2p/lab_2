7d4abea792963dd6542da83c560f8c77f1c5cde9
ross2p <rostyslavpasternak@gmail.com>
2025-11-02 19:52:21 +0200

    fix: correct bin path formatting and update logger configuration in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
+#!/usr/bin/env node
+
 import { CommandFactory } from 'nest-commander';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  await CommandFactory.run(AppModule, {
-    logger: ['error', 'warn'],
-  });
+  await CommandFactory.run(AppModule, ['warn', 'error']);
 }
 
 void bootstrap();
